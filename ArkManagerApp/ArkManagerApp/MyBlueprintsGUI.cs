f17265eeb7b6a5b753708425f1d9ecd7b1dc68b6
ChaseRoth <chasewilliam1999@gmail.com>
2019-07-04 07:16:20 -0400

    Started my blueprints tab

---
+++ ArkManagerApp/ArkManagerApp/MyBlueprintsGUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArkManager.CustomType;
+using ArkManager;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ArkManagerApp
+{
+    public struct MyBlueprintsGUI
+    {
+        public static void PlaceHolder(BlueprintSearchedArgs e)
+        {
+            MessageBox.Show("MyBlueprints invoked");
+        }
+    }
+}
